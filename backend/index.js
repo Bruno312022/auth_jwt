fc3a08d26bf4e7ee881a9395165e3f66dd361027
bruno.falcao <brunocesar.sf.16@gmail.com>
2025-11-27 21:07:33 -0400

    database conection configured

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const sequelize = require("./Config/database");
 const port = 3001;
 const app = express();
 app.use(express.json());
 
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.send("Running")
-} )
+})
 
-app.listen(port, () => {
-    console.log(`Server running on: http://locahost:${port}`)
-});
+sequelize.
+sync().then(() => {
+    console.log("Database connected")
+    app.listen(port, () => {
+        console.log(`Server running on: http://locahost:${port}`)
+    });
+})
